b70b82725423de7a2cbe638de59ac6e3ea0afa8c
Philipp Brendel <waldrumpus@gmail.com>
2013-03-06 10:54:13 +0100

    Add: apply squares to senses

--- lib/wumpus/square.rb
+++ lib/wumpus/square.rb
@@ -1,4 +1,13 @@
 
 class Square
   attr_accessor :gold, :wumpus, :pit, :start
+  
+  def apply_neighbor(senses)
+    senses.stench |= self.wumpus
+    senses.breeze |= self.pit
+  end
+  
+  def apply_center(senses)
+    senses.glitter = self.gold
+  end
 end
--- test/test_square.rb
+++ test/test_square.rb
@@ -4,6 +4,7 @@ require 'wumpus/square'
 class TestSquare < Test::Unit::TestCase
   def setup
     @s = Square.new
+    @r = MockSenses.new
   end
   
   def test_default_values
@@ -13,7 +14,7 @@ class TestSquare < Test::Unit::TestCase
     assert(!@s.start)
   end
   
-  def test_gold
+  def test_accessors
     @s.gold = true
     @s.wumpus = true
     @s.pit = true
@@ -23,5 +24,55 @@ class TestSquare < Test::Unit::TestCase
     assert(@s.wumpus)
     assert(@s.pit)
     assert(@s.start)
+  end
+  
+  def test_apply_neighbor_wumpus
+    @s.wumpus = true
+    @s.apply_neighbor(@r)
+    assert(@r.stench)
+    assert(!@r.breeze)
+    assert(!@r.glitter)
+  end
+  
+  def test_apply_neighbor_pit    
+    @s.pit = true
+    @s.apply_neighbor(@r)
+    assert(@r.breeze)
+    assert(!@r.stench)
+    assert(!@r.glitter)
+  end
+    
+  def test_apply_neighbor_gold
+    @s.gold = true
+    @s.apply_neighbor(@r)
+    assert(!@r.glitter)
+  end
+  
+  def test_apply_center_wumpus
+    @s.wumpus = true
+    @s.apply_center(@r)
+    assert(!@r.stench)
+    assert(!@r.breeze)
+    assert(!@r.glitter)
+  end
+  
+  def test_apply_center_pit
+    @s.pit = true
+    @s.apply_center(@r)
+    assert(!@r.stench)
+    assert(!@r.breeze)
+    assert(!@r.glitter)
+  end
+  
+  def test_apply_center_gold
+    @s.gold = true
+    @s.apply_center(@r)
+    assert(!@r.stench)
+    assert(!@r.breeze)
+    assert(@r.glitter)
+  end
+  
+  class MockSenses
+    attr_accessor :breeze, :stench, :bump, :glitter, :scream
   end
 end
