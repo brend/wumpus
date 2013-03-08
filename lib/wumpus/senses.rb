75a0bd8c3a0e06ed2860775a8e471b66720dd2a7
Philipp Brendel <waldrumpus@gmail.com>
2013-03-08 17:21:10 +0100

    Add: Senses#to_s

--- lib/wumpus/senses.rb
+++ lib/wumpus/senses.rb
@@ -8,6 +8,19 @@ module WumpusHunt
       @glitter = false
       @bump = false
       @scream = false
-    end  
+    end
+    
+    def to_s
+      b = breeze ? "a breeze" : nil
+      s = stench ? "a stench" : nil
+      g = glitter ? "a glitter" : nil
+      p = bump ? "a bump" : nil
+      c = scream ? "a scream" : nil
+      all = [c,p,s,b,g].compact
+      
+      return '[nothing]' if all.empty?
+      
+      "[#{all.join(', ')}]"
+    end
   end
 end
