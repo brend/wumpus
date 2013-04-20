0e002ade2ced7e5bf166d13c16b2b533f3bb0c7c
Philipp Brendel <waldrumpus@gmail.com>
2013-04-20 17:36:40 +0200

    add: begin implementation of replay

---
+++ lib/wumpus/replay.rb
@@ -0,0 +1,30 @@
+class Replay
+  @replays = []
+  
+  def Replay.replays
+    @replays
+  end
+  
+  def Replay.add(world, action_map)
+    @replays << Replay.new(world, action_map)
+  end
+  
+  def Replay.clear
+    @replays.clear
+  end
+  
+  attr_reader :world, :action_map
+  
+  def initialize(world, action_map)
+    @world = world
+    @action_map = action_map
+  end
+end
+
+def clear_replays
+  Replay.clear
+end
+
+def add_replay(world, action_map)
+  Replay.add world, action_map
+end
--- spec/replay_spec.rb
+++ spec/replay_spec.rb
@@ -2,6 +2,27 @@ require 'wumpus/replay'
 
 describe Replay do
   it "defines a method that builds a list of replays" do
-    replay nil, nil
+    add_replay nil, nil
+  end
+  
+  it "provides a list of replays" do
+    Replay.replays
+  end
+  
+  it "adds replays to the list" do
+    world = [[1,0], [2,3], [[0,1], [[2,2], [2,3]]]]
+    action_map = {'Bronco' => [:turn, :forward, :turn, :turn, :forward, :shoot]}
+
+    clear_replays
+    add_replay world, action_map               
+    Replay.replays.nil?.should be_false
+    Replay.replays.length.should eq 1
+    
+    r = Replay.replays.first
+    r.world.should eq world
+    r.action_map.should eq action_map
+    
+    clear_replays
+    Replay.replays.length.should eq 0
   end
 end
